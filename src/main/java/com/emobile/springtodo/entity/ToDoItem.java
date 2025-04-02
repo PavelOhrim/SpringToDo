d006e3bcc740191b0d1a9f947df341e3a79b2832
pavel <pavelokhrimchuk@yandex.ru>
2025-04-02 14:17:41 +0300

    fix: doc added

--- src/main/java/com/emobile/springtodo/entity/ToDoItem.java
+++ src/main/java/com/emobile/springtodo/entity/ToDoItem.java
@@ -7,6 +7,9 @@ import java.time.LocalDateTime;
 
 /**
  * Класс, представляющий задачу (ToDoItem) в системе.
+ * Содержит информацию о задаче, такую как идентификатор, название, описание, статус выполнения и дата создания.
+ *
+ * @author PavelOkhrimchuk
  */
 @Entity
 @Table(name = "todo_items")
@@ -16,17 +19,32 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class ToDoItem {
 
+    /**
+     * Уникальный идентификатор задачи.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * Название задачи.
+     */
     @Column(nullable = false)
     private String title;
 
+    /**
+     * Описание задачи.
+     */
     private String description;
 
+    /**
+     * Статус выполнения задачи.
+     */
     private boolean completed;
 
+    /**
+     * Дата и время создания задачи.
+     */
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 }
