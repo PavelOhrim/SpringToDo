e7f6fb80cdfb5eaeaf123fd44f7ee1de2dea39c0
pavel <pavelokhrimchuk@yandex.ru>
2025-04-11 20:27:28 +0300

    fix

--- src/main/java/com/emobile/springtodo/SpringToDoApplication.java
+++ src/main/java/com/emobile/springtodo/SpringToDoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Главный класс приложения Spring Boot для ToDo.
+ * Главный класс приложения Spring Boot для ToDo
  * Запускает приложение
  *
  * @author PavelOkhrimchuk
